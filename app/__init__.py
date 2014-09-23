3e45a19d8aedf20f7af7baabe7376a7ce862c30e
dualcnhq <nhq1829@gmail.com>
2014-09-23 10:51:00 +0800

    added logging

--- app/__init__.py
+++ app/__init__.py
@@ -13,8 +13,6 @@ lm.init_app(app)
 lm.login_view = 'login'
 oid = OpenID(app, os.path.join(basedir, 'tmp'))
 
-from app import views, models
-
 if not app.debug:
     import logging
     from logging.handlers import SMTPHandler
@@ -24,4 +22,16 @@ if not app.debug:
     mail_handler = SMTPHandler((MAIL_SERVER, MAIL_PORT), 'no-reply@' + MAIL_SERVER, ADMINS, 'microblog failure', credentials)
     mail_handler.setLevel(logging.ERROR)
     app.logger.addHandler(mail_handler)
+
+if not app.debug:
+    import logging
+    from logging.handlers import RotatingFileHandler
+    file_handler = RotatingFileHandler('tmp/microblog.log', 'a', 1 * 1024 * 1024, 10)
+    file_handler.setLevel(logging.INFO)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    app.logger.addHandler(file_handler)
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('microblog startup')
+
+from app import views, models
 
